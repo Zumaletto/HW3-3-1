bcf80f2ef46463e6fa95df9b2124ce995f1fb229
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-11 20:45:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 81325;
+        int numberRubles = 20;
+        int fixedMiles = ticketPrice / numberRubles;
+        System.out.println(fixedMiles);
+    }
+
+}
